ff54556f262b01d00f977887e0181d8a91bfad9a
Roberta Tedde <roby.roby1993@yahoo.com>
2022-05-29 22:09:14 +0200

    Esercizio 01 props

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Welcome } from './Welcome';
+
+
+/* function App(){
+    return (
+        <div>
+        <Hello/>
+        </div>
+    )
+}
+export default App */
+
+
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+              
+                <Welcome name=  {'gianni'}/>
+                
+            </div>
+        )
+    }
+}
