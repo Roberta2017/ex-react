aaa20f12bbf1b4514714594c374ed05f5747e32c
Roberta Tedde <roby.roby1993@yahoo.com>
2022-05-29 21:18:19 +0200

    Esercizio 02 components

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Hello extends React.Components{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
