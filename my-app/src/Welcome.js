5abf8dec10936dc509054527c4edcabe66037cef
Roberta Tedde <roby.roby1993@yahoo.com>
2022-05-30 13:35:52 +0200

    esercizio 04 conditional rendering

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,15 +3,25 @@ import { Age } from './Age'
 
 export class Welcome extends React.Component{
     render(){
+    let scelta;
+    
+    
+        if(this.props.name==='John'){
+           scelta=  <p> Welcome {this.props.name}</p>
+        }
         return(
             <p>
-        welcome {this.props.name}
+
+         {scelta}
+     {this.props.name==='John'&&  <p> Welcome {this.props.name}</p> }
             
           
-        {((this.props.age > 18) && (this.props.age < 65)) && <Age age = {this.props.age}/>}
+      
+        {(this.props.age > 18 && this.props.age < 65) && <Age age = {this.props.age}/>}
 
         </p>
         )
     }
 
 }
+ 
