00b185402f9ec081a0631dc2529b60ff08dc3209
Roberta Tedde <roby.roby1993@yahoo.com>
2022-05-30 06:48:15 +0200

    esercizio 03 props

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,9 +3,16 @@ import React from "react";
 export class Welcome extends React.Component{
     render(){
         return(
-            <p>
-                Hello {this.props.name}
+            <div>
+                  <p>
+                Hello {this.props.name} 
             </p>
+            <p>
+                Your age is {this.props.age}
+            </p>
+            
+            </div>
+          
         )
     }
 }
