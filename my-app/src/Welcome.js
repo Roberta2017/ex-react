acdb567f681bbc935dc335fe62299138cae9efff
Roberta Tedde <roby.roby1993@yahoo.com>
2022-05-31 09:23:02 +0200

    modo2 esercizio 04

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -3,23 +3,23 @@ import { Age } from './Age'
 
 export class Welcome extends React.Component{
     render(){
-    let scelta;
+    /* let scelta;
     
     
         if(this.props.name==='John'){
            scelta=  <p> Welcome {this.props.name}</p>
-        }
+        } */
         return(
-            <p>
+            <span>
 
-         {scelta}
-     {this.props.name==='John'&&  <p> Welcome {this.props.name}</p> }
+        
+     Welcome {this.props.name==='John'&& <span> {this.props.name}</span> }
             
           
       
         {(this.props.age > 18 && this.props.age < 65) && <Age age = {this.props.age}/>}
 
-        </p>
+        </span>
         )
     }
 
