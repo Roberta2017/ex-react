45f6e0646dfc57a8eeebf56f6d7000f39413e5d5
Roberta Tedde <roby.roby1993@yahoo.com>
2022-06-12 14:44:35 +0200

    esercizio 05 list

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,21 +1,29 @@
 import React from "react";
 
+
+
 export class TodoList extends React.Component{
+    state={
+        items: [''],
+    } 
+    buttonOnClick = (event) =>{
+        event.preventDefault()
+        this.setState({
+            items: [...this.state.items, event.target.elements.items.value],
 
-    state = {
-        items : [''],
-    }
-buttonOnClick = (event => {
-    event.preventDefault()
-    
-    this.setState({
-        items: [...this.state.items, event.target.elements.items.value],
+        })
 
-    })
+       
 
-    event.target.reset()
+        event.target.reset() 
+     }
+     handleReset = () => {
+        this.setState({
+          items: [''],
+        });
+      };
 
-})
+   
 
 
 
@@ -28,6 +36,7 @@ buttonOnClick = (event => {
                 <form onSubmit = {this.buttonOnClick}>
                     <input name="items" type="text" />
                     <button type ="submit">Click me</button>
+                    <button onClick={this.handleReset} type ="submit">reset me!</button>
 
                 </form>
 
