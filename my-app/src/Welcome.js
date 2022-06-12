b82b7760e812ba63d8180db6cb5bfd8970493948
Roberta Tedde <roby.roby1993@yahoo.com>
2022-06-12 15:35:43 +0200

    esercizio 1 styling

---
+++ my-app/src/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Welcome extends React.Component{
+    render(){
+        return(
+            <div className="welcome">
+                <p>Hello World</p>
+
+            </div>
+        )
+    }
+}
