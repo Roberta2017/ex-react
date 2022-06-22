87641ee5d6396abe836bca50a66e1de2403a90aa
Roberta Tedde <roby.roby1993@yahoo.com>
2022-06-22 18:44:47 +0200

    esercizio 04

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link} from 'react-router-dom';
 import {ShowGitHubUser} from './ShowGitHubUser';
-
+import { Welcome } from './Welcome';
+import Counter from './Counter'
 
 
 
 export function App(){
   return(
+    <>
     <div>
       <Routes>
         <Route path ='/:username' element ={<ShowGitHubUser />} />
+        <Route path ='/' element={<Welcome />} />
+        <Route path ='/counter' element={<Counter/>} />
       </Routes>
     </div>
+    <div>
+      <Link to="/counter">Counter</Link>
+      <Link to ="/githubuser/Roberta2017"> Git Profile</Link>
+      <Link to="/">Welcome</Link>
+    </div>
+    </>
+    
   )
 }
 
